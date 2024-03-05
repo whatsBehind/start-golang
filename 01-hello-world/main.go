f7bef0849ae7733bfb1a796a82ec97752a4fce03
puyanh <puyanh18@gmail.com>
2024-03-04 17:29:52 -0800

    hello world

---
+++ 01-hello-world/main.go
@@ -0,0 +1,11 @@
+// "go mod init hello-world" to initiate a module in golang
+// "go run main.go" to run the module
+// "go help" manual for golang
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
